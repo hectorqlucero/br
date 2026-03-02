b39c38b1bd364ac2085a3d43e42144c5045f735c
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2026-03-02 11:43:28 -0800

    changed welcome message

--- src/br/handlers/home/controller.clj
+++ src/br/handlers/home/controller.clj
@@ -50,7 +50,7 @@
 
 (defn home
   [request]
-  (let [title "Bienvenido a Bienes Raices Mexicali"
+  (let [title "Bienvenido Ruiz Lucero REALTY & INVEST"
         ok (get-session-id request)
         js nil
         content (private-home title)]
